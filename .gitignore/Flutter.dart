e7fe6c6b2568ba5df170337ad068bfa9163e687d
Rusu Ionela <153896402+Rusu-Ionela@users.noreply.github.com>
2025-09-10 09:02:20 +0000

    Initial commit: Calculator viteză medie (Flutter)

---
+++ .gitignore/Flutter.dart
@@ -0,0 +1,29 @@
+# Flutter/Dart
+.dart_tool/
+.packages
+.pub-cache/
+build/
+.flutter-plugins
+.flutter-plugins-dependencies
+.melos_tool/
+
+# IDE
+.idea/
+.vscode/
+*.iml
+
+# Sistem
+.DS_Store
+Thumbs.db
+
+# Android (artefacte sensibile / temporare)
+android/app/debug
+android/app/profile
+android/app/release
+
+# iOS
+ios/Flutter/Flutter.podspec
+ios/Pods/
+ios/.symlinks/
+ios/Flutter/Generated.xcconfig
+Runner.xcworkspace/
